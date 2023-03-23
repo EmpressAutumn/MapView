621d7a01b6e5cce9a8847d320c90164602000082
Pale-Champion-Atom596 <adamhoffensetz@gmail.com>
2023-03-22 20:41:09 -0400

    Add read_map() from ToTheEnd
    Basic, non-functioning map reader taken from previous project. Will make the code work properly later.

--- main.py
+++ main.py
@@ -1,19 +1,63 @@
+import argparse
 import json
+import numpy as np
 import pygame
-import sys
+
+
+class MapData:
+    def __init__(self):
+        self.countries = []
+
+    def add_country(self, country):
+        self.countries.append(country)
+
+
+class Region:
+    def __init__(self, name, prefix="", suffix="", color="", government="", leader=""):
+        self.name = name
+        if prefix is not None:
+            self.prefix = prefix
+        if suffix is not None:
+            self.suffix = suffix
+        if color is None:
+            self.color = list(np.random.choice(range(256), size=3))  # Generates a random color value, may be changed later
+        if government is not None:
+            self.government = government
+        if leader is not None:
+            self.leader = leader
+        self.consts = []
+        self.coords = []
+
+    def add_const(self, constituency):
+        self.consts.append(constituency)
+
+    def add_coord(self, coordinate):
+        self.coords.append(coordinate)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--filename", type=str)
 
 
 BACKGROUND = (0, 0, 0)
 
 
-def draw_map(screen, map_data, o_x, o_y):
-    pass
+def coords_alterer(coords, o_x, o_y):
+    for i in range(len(coords)):
+        coords[i] += (o_x, o_y)
+    return coords
 
 
-def map_handler(map_data):
+def draw_map(screen, md, o_x, o_y):
+    for i in range(0, len(md.countries) - 1):
+        color = md.countries[i].color
+        pygame.draw.polygon(screen, color, coords_alterer(md.countries[i].coords, o_x, o_y))
+
+
+def map_handler(md):
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption(f"MapView {json.load(open('properties.json'))['version']} by Pale Champion Atom596")
+    pygame.display.set_caption(f"MapView {json.load(open('properties.json'))['version']}")
 
     running = True
     panning = False
@@ -37,26 +81,34 @@ def map_handler(map_data):
                 o_tx, o_ty = event.pos
 
         screen.fill(BACKGROUND)
-        draw_map(screen, map_data, o_x, o_y)
+        draw_map(screen, md, o_x, o_y)
         pygame.display.update()
 
 
-def read_map(file_name):
-    print("reading", file_name)  # Will combine JSON reader from other code
-    return [[0, 0], [0, 1], [1, 1], [1, 0]]
+def country_read_handler(raw_data):
+    base_level = True
+    for key, value in raw_data.items():
+        if key == "constituencies":
+            base_level = False
+
+
+def read_map(fn):
+    if fn is None:
+        fn = input("File to be opened: ")
+    file = json.load(open("map/" + fn))
+    md = MapData()
+    for key, value in file.items():
+        if key == "structure":
+            print(fn + " using file structure " + str(value))
+        else:
+            country_read_handler(value)
+            md.add_country("")
+    return md
 
 
 if __name__ == "__main__":
-    file_name = None
-    for place in range(1, len(sys.argv), 2):
-        if sys.argv[place] in ["-f"]:
-            try:
-                if sys.argv[place] == "-f":
-                    file = sys.argv[place + 1]
-            except IndexError:
-                raise ValueError(f"{sys.argv[place]} has no argument")
-        else:
-            raise ValueError(f"{sys.argv[place]} is not a valid argument option")
+    args = parser.parse_args()
+    file_name = args.filename
 
     map_data = read_map(file_name)
     map_handler(map_data)
