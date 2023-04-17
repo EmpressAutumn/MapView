0c81cfbcbd37120f3dc695b93d49e8acea30a73a
= <adamhoffensetz@gmail.com>
2023-04-17 16:12:57 -0400

    Wrote load_map() Function
    This function returns the province map as a 2D array of tuples, which represent the color of each pixel

--- maploader.py
+++ maploader.py
@@ -1,12 +1,21 @@
 import json
-import png
-import pygame
-import pygame_gui
+import imageio.v3 as iio
+import numpy as np
 
 
-def load_png():
-    pass
+def map_to_json(provinces):
+    md = {}
+    return md
+
+
+def load_map():
+    provinces = iio.imread("assets/textures/map/earth.png").tolist()
+    for y in range(len(provinces)):
+        for x in range(len(provinces[y])):
+            provinces[y][x] = tuple(provinces[y][x])
+    return provinces
 
 
 if __name__ == "__main__":
-    print("Running VisMapReader")
+    prov = load_map()
+    map_data = map_to_json(prov)
