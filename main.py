72e3ab84e1e67006cfbbc57ffc539976f1f9184e
= <adamhoffensetz@gmail.com>
2023-05-15 11:14:01 -0400

    Merged VisMapReader into main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import copy
+import imageio.v3 as iio
 import json
 import numpy as np
 import pygame
@@ -28,18 +29,14 @@ class Region:
             self.color = list(np.random.choice(range(256), size=3))  # Generates a random color value, will change later
         self.government = country_data["government"]
         self.leader = country_data["leader"]
-        self.consts = []
-        self.coords = []
+        self.consts = ["consts"]
+        self.coords = country_data["coords"]
 
     def add_const(self, constituency):
         self.consts.append(constituency)
 
     def add_coord(self, coordinate):
         self.coords.append(coordinate)
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--filename", type=str)
 
 
 BACKGROUND = (31, 128, 128)
@@ -186,6 +183,70 @@ def map_handler(md):
         pygame.display.update()
 
 
+def merge_provinces(prov_list):
+    done = False
+    md = []
+    done_colors = []
+    while not done:
+        color_list = []
+        current_color = None
+        for y in range(len(prov_list)):
+            for x in range(len(prov_list[y])):
+                if prov_list[y][x].color not in done_colors and not None:
+                    current_color = prov_list[y][x].color
+                    color_list.append(prov_list[y][x])
+                    prov_list[y][x].color = None
+        if current_color is not None:
+            done_colors.append(current_color)
+            polygons = []
+            for country in color_list:
+                polygons.append(Polygon(country.coords))
+            x, y = unary_union(polygons).exterior.coords.xy
+            coords = []
+            for i in range(len(x)):
+                coords.append((x[i], y[i]))
+            color_list[0].coords = coords
+            md.append(color_list[0])
+        else:
+            done = True
+    return md
+
+
+def map_to_json(provinces):
+    prov_list = []
+    for y in range(len(provinces)):
+        row = []
+        for x in range(len(provinces[y])):
+            row.append(Region({
+                "name": f"{x}, {y}",
+                "prefix": None,
+                "suffix": None,
+                "color": provinces[y][x],
+                "government": None,
+                "leader": None,
+                "coords": [(x, y), (x + 1, y), (x + 1, y + 1), (x, y + 1)]
+            }))
+        prov_list.append(row)
+    return prov_list
+
+
+def load_map():
+    provinces = iio.imread("assets/textures/map/earth.png").tolist()
+    for y in range(len(provinces)):
+        for x in range(len(provinces[y])):
+            provinces[y][x] = tuple(provinces[y][x])
+    return provinces
+
+
+def maploader_handler():
+    provinces = load_map()
+    prov_list = map_to_json(provinces)
+    md = MapData()
+    for province in merge_provinces(prov_list):
+        md.add_country(province)
+    return md
+
+
 def read_country(count):
     count_data = {"name": None, "prefix": None, "suffix": None, "color": None, "government": None, "leader": None}
     for key, value in count.items():
@@ -193,6 +254,8 @@ def read_country(count):
             count_data[key] = count[key]
         except KeyError:
             pass
+    count_data["coords"] = []
+    count_data["consts"] = []
     country = Region(count_data)
     try:
         for coords in count["coordinates"]:
@@ -206,11 +269,18 @@ def read_country(count):
 def read_map(fn):
     if fn is None:
         fn = input("File to be opened: ")
-    file = json.load(open("map/" + fn))
-    md = MapData()
-    for count in file["map_data"]:
-        md.add_country(read_country(count))
-    return md
+    file = json.load(open("data/map/" + fn))
+    if file["structure"] != -1:
+        md = MapData()
+        for count in file["map_data"]:
+            md.add_country(read_country(count))
+        return md
+    else:
+        return maploader_handler()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--filename", type=str)
 
 
 if __name__ == "__main__":
